f25c4cbd5af0b5b43079d4345504122ada1f4c08
Riley Paulsen <rileypaulsen.bsu@gmail.com>
2015-12-09 01:29:48 -0500

    Adds sourcemaps Updated autoprefixer settings for more sensible version support JS destination path is now a variable

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
 	frontend:{
@@ -12,6 +13,7 @@ var paths = {
 		},
 		js: {
 			src: ['./assets/js/**/*.js', '!./assets/js/app.js', '!./assets/js/libraries/**/*.js'],
+			dest: './assets/js/',
 			filename:'app.min.js'
 		}
 	}
@@ -20,16 +22,20 @@ var paths = {
 gulp.task('scripts', function() {
 	console.log('Concatenating Scripts');
 	return gulp.src(paths.frontend.js.src)
+		.pipe(sourcemaps.init())
 		.pipe(concat(paths.frontend.js.filename))
 		.pipe(uglify())
-		.pipe(gulp.dest('./assets/js/'));
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.frontend.js.dest));
 });
 
 gulp.task('styles', function(){
 	console.log('Compiling Sass');
 	gulp.src(paths.frontend.sass.src)
+		.pipe(sourcemaps.init())
 		.pipe(sass({outputStyle: 'expanded'}).on('error',handleSASSError))
-		.pipe(prefix("last 10 versions", "> 1%", "ie 8", "ie 7"))
+		.pipe(sourcemaps.write())
+		.pipe(prefix("last 2 versions", "> 1%", "ie 8", "ie 7"))
 		.pipe(gulp.dest(paths.frontend.sass.dest));
 });
 
